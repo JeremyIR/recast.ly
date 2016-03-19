4cf55f3cbf7fe7d63f6c9dea1392ffa3e2afdff9
JeremyIR <jeremy.i.rosenfeld@gmail.com>
2016-03-19 12:05:38 -0700

    complete ajax request for searchYoutube.js

--- client/lib/searchYouTube.js
+++ client/lib/searchYouTube.js
@@ -1,5 +1,15 @@
 var searchYouTube = (options, callback) => {
-  // TODO
+  $.ajax({
+    type: 'GET',
+    url: 'https://www.googleapis.com/youtube/v3/search',
+    data: options, 
+    // $.extend({
+    //   key: 'API_KEY',
+    //   q: 'cat vidoes',
+    //   part: 'snippet'
+    // }),
+    success: callback,
+  });
 };
 
 window.searchYouTube = searchYouTube;
