ecc98481aac9c3d385415316190b968a458d06f2
JeremyIR <jeremy.i.rosenfeld@gmail.com>
2016-03-19 10:43:07 -0700

    pass down method to VideoList Entry, pass currentVideo to VideoPlayer

--- client/components/App.js
+++ client/components/App.js
@@ -3,16 +3,16 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      currentVideo: undefined,
+      currentVideo: this.props.videos[0],
       videoList: this.props.videos
     };
   }
 
   changeCurrentVideo(video) {
-    console.log(this);
     this.setState({
       currentVideo: video
-    }).bind;
+    });
+    console.log(this);
   }
 
   render() {
@@ -20,7 +20,7 @@ class App extends React.Component {
       <div>
         <Nav />
         <div className="col-md-7">
-          <VideoPlayer video={this.state.videoList[0]}/>
+          <VideoPlayer video={this.state.currentVideo}/>
         </div>
         <div className="col-md-5">
           <VideoList videos={this.state.videoList} parentProps={this.changeCurrentVideo.bind(this)} />
