835c2d5fd914e9259eb9e3675cd860026f680182
JeremyIR <jeremy.i.rosenfeld@gmail.com>
2016-03-19 17:01:09 -0700

    on keyup, debounces calls search every 1000 ms, completes bare minimum requirements

--- client/components/Search.js
+++ client/components/Search.js
@@ -1,6 +1,11 @@
-var Search = () => (
+var Search = (props) => (
   <div className="search-bar form-inline">
-    <input className="form-control" type="text" />
+    <input className="form-control" id="searchBar" type="text" onKeyUp={
+      _.debounce(
+        function() {
+          props.search(document.getElementById('searchBar').value);
+        }, 1000)
+    } />
     <button className="btn hidden-sm-down">
       <span className="glyphicon glyphicon-search"></span>
     </button>
